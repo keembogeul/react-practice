ded63df0fc1a04d1cb595bc4438a098a831f44e8
keembogeul <318qhtmf@naver.com>
2023-02-22 01:36:06 +0900

    브랜치 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+  // eslint-disable-next-line react/jsx-filename-extension
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
